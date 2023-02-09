c5d997848dccf7dc73a1350a5848a31cf1ddfda6
Robin Heidenis <robin.heidenis@gmail.com>
2023-02-09 23:25:22 +0100

    refactor: 💡 updated styling for list index header
    ✅ Closes: BUC-34

--- src/components/lists/ListCard.tsx
+++ src/components/lists/ListCard.tsx
@@ -1,26 +1,21 @@
 import type { List, ListItem } from '@prisma/client';
 import { ListCardWrapper } from './ListCardWrapper';
-import { useRouter } from 'next/router';
 
 interface ListCardProps {
   list: List & { items: ListItem[] };
 }
 
-export const ListCard = ({ list }: ListCardProps) => {
-  const router = useRouter();
-
-  return (
-    <ListCardWrapper onClick={() => void router.push(`/lists/${list.id}`)}>
-      <h2 className="card-title line-clamp-2">{list.title}</h2>
-      <p className={`line-clamp-${list.title.length > 21 ? '2' : '3'}`}>
-        {list.description}
+export const ListCard = ({ list }: ListCardProps) => (
+  <ListCardWrapper href={`/lists/${list.id}`}>
+    <h2 className="card-title line-clamp-2">{list.title}</h2>
+    <p className={`line-clamp-${list.title.length > 21 ? '2' : '3'}`}>
+      {list.description}
+    </p>
+    <div className="card-actions">
+      <p className="line-clamp-3">
+        {list.items?.length} items •{' '}
+        {list.items.filter((item) => item.checked).length} checked
       </p>
-      <div className="card-actions">
-        <p className="line-clamp-3">
-          {list.items?.length} items •{' '}
-          {list.items.filter((item) => item.checked).length} checked
-        </p>
-      </div>
-    </ListCardWrapper>
-  );
-};
+    </div>
+  </ListCardWrapper>
+);
--- src/components/lists/ListCardWrapper.tsx
+++ src/components/lists/ListCardWrapper.tsx
@@ -1,17 +1,26 @@
 import type { PropsWithChildren } from 'react';
+import Link from 'next/link';
 
 interface ListCardWrapperProps {
   onClick?: () => void;
+  href?: string;
 }
 
 export const ListCardWrapper = ({
   children,
   onClick,
+  href,
 }: PropsWithChildren<ListCardWrapperProps>) => (
   <article
     className="card h-52 w-72 transform cursor-pointer bg-base-100 shadow-xl transition duration-300 hover:scale-110"
     onClick={onClick}
   >
-    <div className="card-body">{children}</div>
+    {href ? (
+      <Link href={href}>
+        <div className="card-body">{children}</div>
+      </Link>
+    ) : (
+      <div className="card-body">{children}</div>
+    )}
   </article>
 );
