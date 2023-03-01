4919fd051fb8777f17e05b5116ff754802cf49da
Robin Heidenis <robin.heidenis@gmail.com>
2023-03-02 00:22:32 +0100

    feat: 🎸 add collection updating, removed duplicate sorting
    ✅ Closes: BUC-46

--- src/components/list/ListItems.tsx
+++ src/components/list/ListItems.tsx
@@ -1,74 +1,28 @@
 import type { RouterOutputs } from '../../utils/api';
-import { Fragment, useMemo } from 'react';
 import { ListItem } from './ListItem';
-import { useSession } from 'next-auth/react';
 import { Movie } from './Movie';
 import { Collection } from './Collection';
+import { usePermissionsCheck } from '../../hooks/usePermissionsCheck';
+import { useSortedMovieItems } from '../../hooks/useSortedMovieItems';
 
 export const ListItems = ({
   listData,
 }: {
   listData: RouterOutputs['lists']['getList'];
 }) => {
-  const { data } = useSession();
-  const isOwner = data?.user?.id === listData.owner.id;
-  const isCollaborator = useMemo(() => {
-    if (!listData) return false;
-    return listData.collaborators.some(
-      (collaborator) => collaborator.id === data?.user?.id,
-    );
-  }, [listData, data?.user?.id]);
-
-  const collections = useMemo(() => {
-    const collections = Object.values(listData.collections).sort((a, b) => {
-      if (a.title < b.title) return -1;
-      if (a.title > b.title) return 1;
-      return 0;
-    });
-    return collections.map((collection) => {
-      collection.items = collection.items.sort((a, b) => {
-        if (a.movie.releaseDate < b.movie.releaseDate) return -1;
-        if (a.movie.releaseDate > b.movie.releaseDate) return 1;
-        return 0;
-      });
-      const allChecked = collection.items.every((item) => item.checked);
-      return { ...collection, allChecked };
-    });
-  }, [listData.collections]);
-
-  const movieItems = useMemo(() => {
-    return [...listData.movieItems, ...collections].sort((a, b) => {
-      let aTitle;
-      if ('movie' in a) {
-        aTitle = a.movie.title;
-      } else {
-        aTitle = a.title;
-      }
-
-      let bTitle;
-      if ('movie' in b) {
-        bTitle = b.movie.title;
-      } else {
-        bTitle = b.title;
-      }
-
-      if (aTitle < bTitle) return -1;
-      if (aTitle > bTitle) return 1;
-      return 0;
-    });
-  }, [listData.movieItems, collections]);
+  const { isOwner, isCollaborator } = usePermissionsCheck(listData);
+  const movieItems = useSortedMovieItems(listData);
 
   return (
     <>
       {listData.type === 'BUCKET'
         ? listData.items.map((item) => (
-            <Fragment key={item.id}>
-              <ListItem
-                isOwner={isOwner}
-                isCollaborator={isCollaborator}
-                {...item}
-              />
-            </Fragment>
+            <ListItem
+              key={item.id}
+              isOwner={isOwner}
+              isCollaborator={isCollaborator}
+              {...item}
+            />
           ))
         : movieItems.map((item) => {
             if ('movie' in item) {
