8acd6c94da4aba2fc18b08c0c46864d5c70360b4
Robin Heidenis <robin.heidenis@gmail.com>
2023-06-20 00:32:26 +0100

    fix: 🐛 maybe fix clerk issues in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/api/webhooks/createUser'],
+  publicRoutes: ['/api/webhooks/createUser', '/'],
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
