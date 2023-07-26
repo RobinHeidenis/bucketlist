3836af6c68246eb45034bdcceef44f98cc88ff71
Robin Heidenis <robin.heidenis@gmail.com>
2023-07-26 02:51:57 +0200

    feat: 🎸 increased default toast time, auth pages styling
    ✅ Closes: BUC-61

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,5 +1,12 @@
 import { SignIn } from '@clerk/nextjs';
+import { StandardPage } from '~/components/page/StandardPage';
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <StandardPage>
+      <div className={'flex h-[85vh] flex-col items-center justify-center'}>
+        <SignIn />
+      </div>
+    </StandardPage>
+  );
 }
