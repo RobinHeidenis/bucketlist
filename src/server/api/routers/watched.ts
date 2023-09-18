c48d4714edd1831b0fd4585945bae64c533b9dd5
Robin Heidenis <robin.heidenis@gmail.com>
2023-09-18 21:15:22 +0100

    feat: 🎸 add route to add a watched item to the user's profile
    ✅ Closes: BUC-84

--- src/server/api/routers/watched.ts
+++ src/server/api/routers/watched.ts
@@ -1,7 +1,51 @@
 import { createTRPCRouter, protectedProcedure } from '~/server/api/trpc';
 import { convertImageHash } from '~/server/api/routers/utils/getList/convertImageHash';
+import { z } from 'zod';
 
 export const watchedRouter = createTRPCRouter({
+  markItemAsWatched: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        type: z.enum(['movie', 'episode']),
+        watchedAt: z.date().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      if (input.type === 'movie') {
+        return ctx.prisma.watchedMovie.create({
+          data: {
+            watchedAt: input.watchedAt,
+            movie: {
+              connect: {
+                id: input.id,
+              },
+            },
+            user: {
+              connect: {
+                id: ctx.auth.userId,
+              },
+            },
+          },
+        });
+      }
+
+      return ctx.prisma.watchedEpisode.create({
+        data: {
+          watchedAt: input.watchedAt,
+          episode: {
+            connect: {
+              id: input.id,
+            },
+          },
+          user: {
+            connect: {
+              id: ctx.auth.userId,
+            },
+          },
+        },
+      });
+    }),
   getAllWatchInstances: protectedProcedure.query(async ({ ctx }) => {
     const movies = await ctx.prisma.movie.findMany({
       where: { watchedBy: { some: { userId: ctx.auth.userId } } },
