bbd886dc0b70bd98242bc0cd451cb9e0cf1f2106
Robin Heidenis <robin.heidenis@gmail.com>
2024-03-19 04:38:06 +0000

    fix: 🐛 errors in code due to release date now possibly null

--- src/hooks/filterModes/shows/releaseDate.ts
+++ src/hooks/filterModes/shows/releaseDate.ts
@@ -1,12 +1,16 @@
 import type { ShowListShow } from '~/types/List';
 
 export const sortReleaseDate = (a: ShowListShow, b: ShowListShow) => {
+  if (!a.releaseDate) return 1;
+  if (!b.releaseDate) return -1;
   if (a.releaseDate < b.releaseDate) return 1;
   if (a.releaseDate > b.releaseDate) return -1;
   return 0;
 };
 
 export const sortReleaseDateReverse = (a: ShowListShow, b: ShowListShow) => {
+  if (!a.releaseDate) return -1;
+  if (!b.releaseDate) return 1;
   if (a.releaseDate < b.releaseDate) return -1;
   if (a.releaseDate > b.releaseDate) return 1;
   return 0;
